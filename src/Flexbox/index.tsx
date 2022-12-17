23223a5948e1a476e63bd1c8afa50b3c1bc01f2b
arvinxx <arvinx@foxmail.com>
2022-12-17 19:06:14 +0800

    :sparkles: feat: 使用 gap 替换伪元素选择器实现 gap

--- src/Flexbox/index.tsx
+++ src/Flexbox/index.tsx
@@ -1,4 +1,4 @@
-import type { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import type { DetailedHTMLProps, HTMLAttributes } from 'react';
 import styled from 'styled-components';
 
 import {
@@ -32,25 +32,23 @@ export interface IFlexbox {
   padding?: string | number | CommonSpaceNumber;
   flex?: number | string;
   /**
-   * 是否隐藏内容
+   * 是否显示
    */
-  hidden?: boolean;
+  visible?: boolean;
 }
 
 export type FlexboxProps = IFlexbox &
   DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
 
-export const Flexbox: FC<FlexboxProps> = styled.div.attrs(() => ({
-  className: 'layoutkit-flexbox',
+export const Flexbox = styled.div.attrs<IFlexbox>(() => ({
+  className: 'rlk-flexbox',
 }))<IFlexbox>`
   // 是否显示
-  display: ${(props) => (props.hidden ? 'none' : 'flex')};
+  display: ${(props) => (props.visible ? 'none' : 'flex')};
 
   flex: ${(props) => props.flex};
 
-  flex-direction: ${(props) => {
-    return getFlexDirection(props.direction, props.horizontal);
-  }};
+  flex-direction: ${(props) => getFlexDirection(props.direction, props.horizontal)};
   justify-content: ${(props) => props.distribution};
   align-items: ${(props) => props.align};
 
@@ -68,10 +66,10 @@ export const Flexbox: FC<FlexboxProps> = styled.div.attrs(() => ({
 
   padding: ${(props) => getCssValue(props.padding)};
 
+  gap: ${(props) => getCssValue(props.gap)};
+
   > *:not(:last-child) {
-    margin-right: ${(props) =>
-      isHorizontal(props.direction, props.horizontal) && getCssValue(props.gap)};
-    margin-bottom: ${(props) =>
+    margin-block-end: ${(props) =>
       isVertical(props.direction, props.horizontal) && getCssValue(props.gap)};
   }
 `;
