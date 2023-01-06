4c781684b91d04ac2587a384883e93860ad97f2b
arvinxx <arvinx@foxmail.com>
2023-01-07 00:39:26 +0800

    :bug: fix: export types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Center } from './Center';
 export type { CenterProps } from './Center';
 export { Flexbox } from './Flexbox';
 export type { CommonSpaceNumber, FlexboxProps, IFlexbox } from './Flexbox';
+export * from './type';
