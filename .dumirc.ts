20cffdf5781d03fe904789fc36ea1a8b1802ae09
arvinxx <arvinx@foxmail.com>
2023-03-01 23:15:24 +0800

    :wrench: chore: 优化文档配置

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,16 +1,31 @@
 import { defineConfig } from 'dumi';
+import type { SiteThemeConfig } from 'dumi-theme-antd-style';
 
 import { homepage } from './package.json';
 
-export default defineConfig({
-  themeConfig: {
-    name: 'react-layout-kit',
-    github: homepage,
-    apiHeader: {
-      sourceUrl: `{github}/tree/master/src/{atomId}/index.tsx`,
-      docUrl: `{github}/tree/master/docs/components/{atomId}.md`,
-    },
+const themeConfig: SiteThemeConfig = {
+  name: 'react-layout-kit',
+  github: homepage,
+  apiHeader: {
+    sourceUrl: `{github}/tree/master/src/{atomId}/index.tsx`,
+    docUrl: `{github}/tree/master/docs/components/{atomId}.md`,
   },
+  hero: {
+    actions: [
+      {
+        text: '立即开始',
+        link: '/components/flexbox',
+      },
+    ],
+  },
+  footerConfig: {
+    columns: false,
+    bottom: 'Made by Arvin Xu',
+  },
+};
+
+export default defineConfig({
+  themeConfig,
   apiParser: {},
   resolve: {
     entryFile: './src/index.ts',
--- docs/index.md
+++ docs/index.md
@@ -1,7 +1,4 @@
 ---
 hero:
   title: React <b>Layout Kit</b>
-  actions:
-    - text: 立即开始
-      link: /components/flexbox
 ---
