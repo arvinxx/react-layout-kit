7662d26279f2a5bf870bae9b3cbfea9efdebef5c
arvinxx <arvinx@foxmail.com>
2023-03-28 22:48:32 +0800

    :label: chore: 补充类型定义说明

--- src/type.ts
+++ src/type.ts
@@ -20,6 +20,12 @@ export type FlexDirection = 'vertical' | 'vertical-reverse' | 'horizontal' | 'ho
 
 export type DivProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLDivElement>;
 
+/**
+ * @title 公共属性
+ */
 export interface CommonProps {
+  /**
+   * @title 类名前缀
+   */
   prefixCls?: string;
 }
