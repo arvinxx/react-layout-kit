9e5685a767037976bdde6cb34c274de82f5c7139
arvinxx <arvinx@foxmail.com>
2023-08-13 15:49:47 +0800

    :wrench: chore: update package config

--- .changelogrc.js
+++ .changelogrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   displayTypes: ['feat', 'fix', 'styles', 'pref'],
   titleLanguage: 'zh-CN',
+  showAuthor: true,
+  showAuthorAvatar: true,
+  showSummary: true,
+  reduceHeadingLevel: true,
+  newlineTimestamp: true,
+  addBackToTop: true,
 };
